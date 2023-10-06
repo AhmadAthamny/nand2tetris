b653af9cceeb452f2bbe7edcb72eaa0891c7f227
Ahmad Atamna <ahmed@cs.huji.ac.il>
2023-10-06 15:30:05 +0300

    Swap.asm done.

--- 04/swap/Swap.asm
+++ 04/swap/Swap.asm
@@ -15,3 +15,90 @@
 // - Changing R14, R15 is not allowed.
 
 // Put your code here.
+@R14
+D=M
+@index
+M=D
+@max_index
+M=D
+@min_index
+M=D
+
+(LOOP)
+    @R14
+    D=M
+    @R15
+    D=D+M
+
+    @index
+    D = D-M
+    @UPDATE
+    D;JEQ
+
+    @index
+    A=M
+    D=M
+
+    @max_index
+    A=M
+    D=M-D
+    @UPDATE_MAX
+    D;JLT
+    
+    @index
+    A=M
+    D=M
+    
+    @min_index
+    A=M
+    D=M-D
+    @UPDATE_MIN
+    D;JGE
+
+    @INCREASE
+    0;JMP
+
+    (UPDATE_MAX)
+        @index
+        D=M
+        @max_index
+        M=D
+
+        @INCREASE
+        0;JMP
+
+    (UPDATE_MIN)
+        @index
+        D=M
+        @min_index
+        M=D
+    
+    (INCREASE)
+        @index
+        M=M+1
+        @LOOP
+        0;JMP
+
+(UPDATE)
+    @max_index
+    A=M
+    D=M
+    @tmp
+    M=D
+
+    @min_index
+    A=M
+    D=M
+    @max_index
+    A=M
+    M=D
+
+    @tmp
+    D=M
+    @min_index
+    A=M
+    M=D
+
+(END)
+    @END
+    0;JMP
